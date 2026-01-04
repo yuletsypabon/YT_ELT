abf9997ac831553cd0189c58f8566f142a42e4e8
yuletsy pabon <yuletsyp.pabonf@unac.edu.co>
2026-01-03 21:43:50 -0500

    Added function video_ids into array emmpty and print the list on it

--- video_stats.py
+++ video_stats.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlist_id():
     try:
@@ -23,8 +24,46 @@ def get_playlist_id():
         return Channel_playlistId
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+
+def get_video_ids(playlist_id):
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId={playlist_id}&key={API_KEY}&{maxResults}=60"
+    
+    try:
+        while True:
+            url  = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+            
+        return video_ids
+
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlist_id()
+    #get_playlist_id()
+    playlist_id = get_playlist_id()
+    #get_video_ids(playlist_id)
+    print(get_video_ids(playlist_id))
+
+
     
 
 
